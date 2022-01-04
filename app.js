f2b24f3bd76d020a2d8d03348d5ddb2c9dc2ef2a
Vitor Valandro <vitormateusd@gmail.com>
2022-01-04 11:06:39 -0300

    Implement array view boilerplate:
    Add text, fix bar positions and create Bar class

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const array = new ArrayView([10, 20, 30], 500);
+function randomArray(length) {
+  let arr = [];
+  for (let i = 0; i < length; i++) {
+    let random = Math.floor(Math.random() * (99 - 10)) + 10;
+    arr.push(random)
+  }
+  return arr;
+}
+
+const array = new ArrayView(randomArray(10), 500);
 array.render(document.getElementById("tempboilerplate"));
--- views/ArrayView.js
+++ views/ArrayView.js
@@ -5,6 +5,19 @@ class ArrayView {
     this.canvas = document.createElement('canvas');
     this.canvas.width = size;
     this.canvas.height = size / 2;
+    this.ctx = this.canvas.getContext('2d');
+    this.bars = array.reduce((arr, item, index) => {
+      let bar = new Bar(
+        1 * (index * 45) + 32.5, // x
+        this.canvas.height - (this.canvas.height / 100 * item), // y
+        30, // width
+        this.canvas.height / 100 * item, // heigth
+        item,
+        index
+      );
+      arr.push(bar);
+      return arr;
+    }, []);
   }
 
   render(element) {
@@ -14,11 +27,37 @@ class ArrayView {
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     ctx.fillStyle = "#313C45";
-    this.array.forEach((item, index) => {
-      console.log(item, index)
-      ctx.fillRect(1 * (index * 50), this.canvas.height - item * 5, item, item * 5);
+    this.bars.forEach(bar => {
+      ctx.fillStyle = "#313C45";
+
+      ctx.fillRect(
+        bar.x,
+        bar.y,
+        bar.width,
+        bar.height
+      );
+
+      ctx.fillStyle = 'white';
+      ctx.font = "12px Arial";
+      ctx.fillText(
+        bar.value, // text
+        bar.x + 7.5, // x
+        this.canvas.height - (this.canvas.height / 100 * bar.value) / 2 // y
+      );
     })
 
     element.appendChild(this.canvas);
+  }
+}
+
+class Bar {
+  constructor(x, y, width, height, value, index) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.value = value;
+    this.index = index;
   }
 }
