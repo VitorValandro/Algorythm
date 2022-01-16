2b6cbf0d9cbeb3f8191f14c2bbcb2972066890ed
Vitor Valandro <vitormateusd@gmail.com>
2022-01-16 20:06:14 -0300

    Implements own versions of _indexOf and _slice array methods

--- algorithms/arrays/commons.js
+++ algorithms/arrays/commons.js
@@ -15,15 +15,35 @@ Array.prototype._unshift = function (element) {
 
 Array.prototype._shift = function () {
   // the Array._shift() method implementation
+  const element = this[0];
   for (let i = 0; i < this.length; i++) {
     this[i] = this[i + 1];
   }
   this.length--;
-  return this;
+  return element;
 }
 
 Array.prototype._pop = function () {
   // the Array.pop() method implementation
+  const element = this[this.length - 1];
   this.length--;
-  return this;
+  return element;
+}
+
+Array.prototype._indexOf = function (element) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == element)
+      return i
+  }
+  return -1;
+}
+
+Array.prototype._slice = function (start = 0, end = this.length) {
+  let arr = [];
+  if (start < 0)
+    start += this.length;
+  for (let i = start; i < end; i++) {
+    arr.push(this[i]);
+  }
+  return arr;
 }
