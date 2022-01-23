7b16d13c2bf54bab481cfac890805ae1e248526a
Vitor Valandro <vitormateusd@gmail.com>
2022-01-23 08:29:16 -0300

    Implement the Fibonacci solver recursive algorithm  using memoization for optimization

--- algorithms/other/Fibonacci.js
+++ algorithms/other/Fibonacci.js
@@ -25,3 +25,26 @@ function recursiveFibonacci(n) {
   if (n <= 2) return 1;
   return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 }
+
+function memoizationFibonacci(n) {
+  /* 
+  * Fibonacci solver using recursion and memoization.
+  * Memoization is a optimization method like caching. Values already calculated
+  * are stored in the memo array and reused instead being recalculated.
+  * 
+  * In fibonacci(5), for example, we calculate fibonacci(3) twice, so we can store and reuse the result,
+  * saving some processing later.
+  */
+  if (n < 1) return 0;
+  // memo is our cache. The index is n and the value is the result.
+  const memo = [0, 1];
+
+  const fibonacci = n => {
+    // if fibonacci(n) has already been calculated, use its value
+    if (memo[n] != null) return memo[n];
+    // if not, calculate the value and store it into memo
+    return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
+  }
+
+  return fibonacci(n);
+}
